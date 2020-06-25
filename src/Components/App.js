9e7d05c5a95120e7b434304af1db47749dd95924
Justin Stoddard <justinstoddard@Justins-MacBook-Pro.local>
2020-06-25 13:24:03 -0600

    Added routes

--- src/Components/App.js
+++ src/Components/App.js
@@ -5,14 +5,33 @@ import Home from './Home/Home';
 import Resume from './Resume/Resume';
 import Spacex from './Spacex/Spacex';
 import Pokemon from './Pokemon/Pokemon';
+import VirtualBusinessCard from './VirtualBusinessCard/VirtualBusinessCard';
 import Theme from './Theme';
 import { CssBaseline, ThemeProvider, Grid } from '@material-ui/core';
 
 const App = () => {
+  const isOnVirtualBusinessCardRoute = window.location.pathname === "/virtual-businesscard";
+
   return (
     <ThemeProvider theme={Theme}>
       <CssBaseline />
-      <h1>Hello World!</h1>
+      <Router>
+        <Switch>
+          {isOnVirtualBusinessCardRoute ? (
+            <Route exact path="/virtual-businesscard" component={VirtualBusinessCard} />
+          ) : (
+            <Grid container>
+              <Grid xs={12}>
+                <Navigation />
+              </Grid>
+              <Grid xs={12}>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/home" component={Home} />
+              </Grid>
+            </Grid>
+          )}
+        </Switch>
+      </Router>
     </ThemeProvider>
   );
 };
